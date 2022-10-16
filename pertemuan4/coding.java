441429c2abc3e50328fd4c70099c5ec17cee53a9
Bhatekz <114419039+Bhatekz@users.noreply.github.com>
2022-10-16 23:48:18 +0700

    Add files via upload

---
+++ pertemuan4/coding.java
@@ -0,0 +1,13 @@
+package pertemuan4;
+
+public class coding {
+   
+    public static void main(string[] args){
+        scanner masuk.new scanner (system.in) ;
+    
+        float nama_barang,harga,pajak,total_harga;
+    
+    System.out.PrintIn("Nama Barang     = .");
+    System.out.PrintIn("Harga Barang    = .");
+    System.out.PrintIn("Pajak           = 10%");
+    System.out.PrintIn("Total Bayar     = .");
---
+++ pertemuan4/tugas.java
@@ -0,0 +1,19 @@
+package pertemuan4;
+
+public class tugas {
+   
+    public static void main(String[] args) {
+
+        float nama_barang,harga,pajak,total_harga;
+    { 
+        system.out.printin total_harga =(harga * 0.1f);
+        float total_harga = (harga* pajak);
+        total_harga=(harga*0.1f);
+       
+System.out.printin("Nama Barang     = .");
+System.out.printin("Harga Barang    = .");
+System.out.printin("Pajak           = 10%");
+System.out.printin("Total Bayar     = .");
+    }
+}
+}
